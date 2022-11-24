a69883602b9496f1b555b6d536f2360fc37f9e5e
Joyce <joycenjoroge47@gmail.com>
2022-11-24 12:35:37 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-function sayHelloTo(firstName = "User") {
-    console.log(`Hello, ${firstName}!`);
+function introduction(name) {
+    return `Hi, my name is ${name}.`;
+  }
+
+
+function introductionWithLanguageOptional(name,language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+  }
+
+  function introductionWithLanguage(name,language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+  }
+
+  function introductionWithLanguageOptional(name,language ="JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
   }
